0088660abf790d4f57d8f0173b35203403f03108
nirajcoder373 <133911964+nirajcoder373@users.noreply.github.com>
2024-03-21 16:13:08 +0000

    Datatype

---
+++ 05datatype.js
@@ -0,0 +1,33 @@
+"use strict";
+//treat all js code as new  version
+
+
+//alert("engineer") we are using node js not browser
+console.log(3+3)
+
+console.log("niraj")
+
+
+
+let name ="vikash"
+let age =23
+let isLoggedIn =false
+let state;
+
+// number=> 2 to power 53 
+//bigint 
+
+
+// string=>""=double code=singal code
+
+//boolean =>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+console.log(typeof"niraj")
+console.log(typeof null)//object
+console.log(typeof string)
+console.log(typeof undefined);//undefined
---
+++ tes.js
@@ -0,0 +1,15 @@
+const accountId = "1234"
+let accountEmail ="niraj23@gamil.com"
+let accountPassword ="12345"
+let accountCity ="Bihar"
+
+let accountState ="Haryana"
+
+//
+// accountEmail ="niraj23@gamil.com"
+// accountPassword ="12345"
+// accountCity ="Bihar"
+// accountState ="Haryana"
+// console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
